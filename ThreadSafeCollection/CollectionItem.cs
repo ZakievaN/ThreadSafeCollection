b34c1c1390a4e175069b07077e80a1df03443924
ZakievaN <nafiska.k@gmail.com>
2024-02-21 01:08:04 +0300

    Update CollectionItem.cs
    удалены артефакты

--- ThreadSafeCollection/CollectionItem.cs
+++ ThreadSafeCollection/CollectionItem.cs
@@ -1,7 +1,6 @@
 ﻿
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("friend_signed_B, PublicKey=0024000004800000940000000602000000240000525341310004000001000100254cf0834bc194d4afc36071bc198626456b5db5f4117dea0dc1be02c0ec3c7c530a427299c25a2e01805d84ec996512a62aba3152d9d692a12cec91303719ca3ffb6224ad784498b34da8cf146a281f09f2f8123d71630f34a92d13094641d4f7ff5a449d943d9a56e8e827a02aa07328335d58deabc213d21a949ceb3955aa")]
 namespace ThreadSafeCollection
 {
     internal class CollectionItem<TId, TName, TValue> : IComparable<CollectionItem<TId, TName, TValue>>
